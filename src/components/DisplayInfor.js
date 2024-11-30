86ade9f96fad7eccf304990716daa7f1693b4ded
roymhung <royhung123cxz@gmail.com>
2024-11-30 16:56:02 +0700

    Toggle show and hide with ternary operator

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,6 +2,15 @@ import React from "react";
 import UserInfor from "./UserInfor";
 
 class DisplayInfor extends React.Component {
+  state = {
+    isShowListUser: true,
+  };
+  handleShowHide = () => {
+    this.setState({
+      isShowListUser: !this.state.isShowListUser,
+    });
+  };
+
   render() {
     //destructuring array/object
     //const listusers = this.props.listUsers;
@@ -11,49 +20,31 @@ class DisplayInfor extends React.Component {
     //props => viết tất properties
     return (
       <div>
-        {listUsers.map((user, index) => {
-          console.log(">>> check map user", user);
-
-            return (
-              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                <div>My name's {user.name}</div>
-                <div>My age's {user.age}</div>
-                <hr />
-              </div>
-            );
-
-
-
-
-
-        //   if (+user.age > 18) {
-        //     return (
-        //       <div key={user.id} className="green">
-        //         <div>My name's {user.name}</div>
-        //         <div>My age's {user.age}</div>
-        //         <hr />
-        //       </div>
-        //     );
-        //   } else {
-        //     return (
-        //       <div key={user.id} className="red">
-        //         <div>My name's {user.name}</div>
-        //         <div>My age's {user.age}</div>
-        //         <hr />
-        //       </div>
-        //     );
-        //   }
-        })}
-        {/* <div>My name's {name}</div>
-        <div>My age's {age}</div>
-        <hr />
-
-        <div>My name's {name}</div>
-        <div>My age's {age}</div>
-        <hr />
-
-        <div>My name's {name}</div>
-        <div>My age's {age}</div> */}
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+           
+            {this.state.isShowListUser === true
+              ? "Hide list users:"
+              : "Show list users:"}
+          </span>
+        </div>
+        {this.state.isShowListUser && (
+          <div>
+            {listUsers.map((user, index) => {
+              return (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  <div>My name's {user.name}</div>
+                  <div>My age's {user.age}</div>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
