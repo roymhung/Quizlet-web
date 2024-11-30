6507ba493da85f294fd99a40d8a5c17824937b5e
roymhung <royhung123cxz@gmail.com>
2024-11-30 16:30:19 +0700

    cau dk if else

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -6,18 +6,30 @@ class DisplayInfor extends React.Component {
     //destructuring array/object
     //const listusers = this.props.listUsers;
     const { listUsers } = this.props;
-    console.log(listUsers);
+    // console.log(listUsers);
+    // console.table(listUsers);
     //props => viết tất properties
     return (
       <div>
-        { listUsers.map((user,index) => {
+        {listUsers.map((user, index) => {
+          console.log(">>> check map user", user);
+          if (+user.age > 18) {
             return (
-              <div key={user.id}>
+              <div key={user.id} className="green">
                 <div>My name's {user.name}</div>
                 <div>My age's {user.age}</div>
                 <hr />
               </div>
             );
+          } else {
+            return (
+              <div key={user.id} className="red">
+                <div>My name's {user.name}</div>
+                <div>My age's {user.age}</div>
+                <hr />
+              </div>
+            );
+          }
         })}
         {/* <div>My name's {name}</div>
         <div>My age's {age}</div>
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,7 +8,7 @@ import UserInfor from "./UserInfor";
 class MyComponent extends React.Component {
   state = {
     listUsers: [
-      { id: 1, name: "roy", age: "18" },
+      { id: 1, name: "roy", age: "15" },
       { id: 2, name: "roylee", age: "20" },
       { id: 3, name: "hung", age: "45" },
     ],
