7e8e564ace536d4d68f79002004b114b59c33b87
roymhung <royhung123cxz@gmail.com>
2024-11-30 15:05:25 +0700

    Form in React

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -18,25 +18,34 @@ class MyComponent extends React.Component {
       name: "roy",
       age: Math.floor(Math.random() * 100 + 1),
     });
-
-  }
+  };
 
   handleOnMoverOver(event) {
     // console.log(event.pageX);
   }
+
+  handleOnChangeInput = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state)
+  };
   // jsx
   render() {
     return (
       <div>
         My name is {this.state.name} and I'm age {this.state.age}
-        <button onMouseOver={this.handleOnMoverOver}>Hover me!</button>
-        <button
-          onClick={(event) => {
-            this.handleClick(event);
-          }}
-        >
-          Click me!
-        </button>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnChangeInput(event)}
+          />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
