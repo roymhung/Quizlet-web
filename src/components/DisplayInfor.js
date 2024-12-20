29724bfb6759f2bc4132ce2480912e98815cef11
roymhung <royhung123cxz@gmail.com>
2024-12-20 14:24:05 +0700

    StateFul/Stateless Component

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,64 +1,57 @@
 import React from "react";
 import "./DisplayInfor.scss";
-import logo from "./../logo.svg";
+import logo1 from "./../logo.svg";
 
-class DisplayInfor extends React.Component {
-  constructor(props) {
-    console.log(">>>> call constructor: 1");
-    super(props);
-    // No error because babel compiler
-    this.state = {
-      isShowListUser: true,
-    };
-  }
+//stateless vs stateful
+// class DisplayInfor extends React.Component {
+ 
+//   render() {
+//     console.log(">>> call me render ");
+//     //destructuring array/object
+//     //const listusers = this.props.listUsers;
+//     const { listUsers } = this.props;
+//     // console.log(listUsers);
+//     // console.table(listUsers);
+//     //props => viết tất properties
 
-  componentDidMount() {
-    console.log(">>>>: call me componentDidMount");
-    setTimeout(()=>{
-      document.title = "Quizlet-web";
-    }, 3000);
-  }
+//     //template + logic js
+//     return (
+//       <div className="display-infor-container">
+  
+//         {true && (
+//           <>
+//             {listUsers.map((user, index) => {
+//               return (
+//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                   <div>
+//                     <div>My name's {user.name}</div>
+//                     <div>My age's {user.age}</div>
+//                   </div>
+//                   <div>
+//                     <button
+//                       onClick={() => this.props.handleDeleteUser(user.id)}
+//                     >
+//                       Delete
+//                     </button>
+//                   </div>
+//                   <hr />
+//                 </div>
+//               );
+//             })}
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-  componentDidUpdate(prevProps, prevState, snapshot){
-    console.log(">>> call me component did update", this.props, prevProps);
-    if(this.props.listUsers !== prevProps.listUsers){
-      if(this.props.listUsers.length === 5){
-        alert('you got 5 users')
-      }
-    }
-  }
-
-  handleShowHide = () => {
-    this.setState({
-      isShowListUser: !this.state.isShowListUser,
-    });
-  };
-
-  render() {
-    console.log(">>> call me render ");
-    //destructuring array/object
-    //const listusers = this.props.listUsers;
-    const { listUsers } = this.props;
-    // console.log(listUsers);
-    // console.table(listUsers);
-    //props => viết tất properties
-
-    //template + logic js
+const DisplayInfor = (props) => {  
+  const { listUsers } = props;
+   
     return (
       <div className="display-infor-container">
-        {/* <img src={logo} /> */}
-        <div>
-          <span
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShowListUser === true
-              ? "Hide list users:"
-              : "Show list users:"}
-          </span>
-        </div>
-        {this.state.isShowListUser && (
+  
+        {true && (
           <>
             {listUsers.map((user, index) => {
               return (
@@ -69,7 +62,7 @@ class DisplayInfor extends React.Component {
                   </div>
                   <div>
                     <button
-                      onClick={() => this.props.handleDeleteUser(user.id)}
+                      onClick={() => props.handleDeleteUser(user.id)}
                     >
                       Delete
                     </button>
@@ -83,5 +76,5 @@ class DisplayInfor extends React.Component {
       </div>
     );
   }
-}
+
 export default DisplayInfor;
