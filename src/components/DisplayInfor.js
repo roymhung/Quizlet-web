2ddb3aee1ddeac8702c2fd40e91132dde94444a2
roymhung <royhung123cxz@gmail.com>
2024-12-20 16:39:55 +0700

    useEffect Hook - Using LifeCycle with React Function Component

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,92 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
 
 //stateless vs stateful
-// class DisplayInfor extends React.Component {
- 
-//   render() {
-//     console.log(">>> call me render ");
-//     //destructuring array/object
-//     //const listusers = this.props.listUsers;
-//     const { listUsers } = this.props;
-//     // console.log(listUsers);
-//     // console.table(listUsers);
-//     //props => viết tất properties
 
-//     //template + logic js
-//     return (
-//       <div className="display-infor-container">
-  
-//         {true && (
-//           <>
-//             {listUsers.map((user, index) => {
-//               return (
-//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                   <div>
-//                     <div>My name's {user.name}</div>
-//                     <div>My age's {user.age}</div>
-//                   </div>
-//                   <div>
-//                     <button
-//                       onClick={() => this.props.handleDeleteUser(user.id)}
-//                     >
-//                       Delete
-//                     </button>
-//                   </div>
-//                   <hr />
-//                 </div>
-//               );
-//             })}
-//           </>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
-const DisplayInfor = (props) => {  
+const DisplayInfor = (props) => {
   const { listUsers } = props;
-  
-  const[isShowHideListUser, setShowHideListUser] = useState(true);
+
+  const [isShowHideListUser, setShowHideListUser] = useState(true);
   // this.state = {
   //   isShowHideListUser: true
   // }
   const handleShowHideListUser = () => {
     setShowHideListUser(!isShowHideListUser);
-  }
+  };
 
-    return (
-      <div className="display-infor-container">
-        <div>
-          <span onClick={() => handleShowHideListUser()}>
-            {isShowHideListUser === true
-              ? "Hide list Users"
-              : "Show list Users"}
-          </span>
-        </div>
-        {isShowHideListUser && (
-          <>
-            {listUsers.map((user, index) => {
-              return (
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div>
-                    <div>My name's {user.name}</div>
-                    <div>My age's {user.age}</div>
-                  </div>
-                  <div>
-                    <button onClick={() => props.handleDeleteUser(user.id)}>
-                      Delete
-                    </button>
-                  </div>
-                  <hr />
-                </div>
-              );
-            })}
-          </>
-        )}
+  console.log(">>> call me render");
+
+  useEffect(() => {
+    if (listUsers.length === 0) {
+      alert("you deleted all the users");
+    }
+    console.log(">>> call me useEffect");
+  }, [listUsers]);
+
+  return (
+    <div className="display-infor-container">
+      <div>
+        <span onClick={() => handleShowHideListUser()}>
+          {isShowHideListUser === true ? "Hide list Users" : "Show list Users"}
+        </span>
       </div>
-    );
-  }
+      {isShowHideListUser && (
+        <>
+          {listUsers.map((user, index) => {
+            return (
+              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                <div>
+                  <div>My name's {user.name}</div>
+                  <div>My age's {user.age}</div>
+                </div>
+                <div>
+                  <button onClick={() => props.handleDeleteUser(user.id)}>
+                    Delete
+                  </button>
+                </div>
+                <hr />
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfor;
